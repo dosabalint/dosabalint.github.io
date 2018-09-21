70bcdf4a36075a9ce1c89a0acdf377c0d7bfc760
dosabalint <dosa.balint@gmail.com>
2018-09-21 20:46:08 +0200

    link update, md import

---
+++ md/index.md
@@ -0,0 +1,13 @@
+# Content so far
+
+## Drupal related
+
+### PHP
+
+* [Design patterns](https://phptherightway.com/pages/Design-Patterns.html)
+
+### Drupal 8
+
+* [Rest resource](http://enzolutions.com/articles/2014/12/16/how-to-create-a-rest-resource-in-drupal-8/)
+* [Modular authentication system](https://www.drupal.org/node/2031999)
+* [Authentication, Authorization](https://2014.drupalcamp.es/sites/default/files/slides/Authentication%20in%20Drupal%208.pdf)
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,17 @@
-<template lang="md">
-
-# Content so far
-
-## Drupal related
-
-### PHP
-
-* [Design patterns](https://phptherightway.com/pages/Design-Patterns.html)
-
-### Drupal 8
-
-* [Rest resource](http://enzolutions.com/articles/2014/12/16/how-to-create-a-rest-resource-in-drupal-8/)
-* https://www.drupal.org/node/2031999
-* https://2014.drupalcamp.es/sites/default/files/slides/Authentication%20in%20Drupal%208.pdf
-
+<template>
+<div v-html="content"></div>
 </template>
 
 <script>
-  export default {}
+  import content from "~/md/index.md";
+
+  export default {
+    computed: {
+      content(){
+        return content;
+      }
+    }
+  }
 </script>
 
 <style>
